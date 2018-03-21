2dab442fbdb1db27ab8e0db48954610123b01b5b
Nix2k <mihail.bokov@gmail.com>
2018-03-21 17:51:28 +0300

    v0.1

--- create_tab.php
+++ create_tab.php
@@ -10,7 +10,7 @@
 		echo "<h2>Структура таблицы </h2>$name";
 		echo '<form action="create_tab.php" method="GET">'
 		echo '<input type="hidden" name="cols" value="'.$cols.'"><br>';
-		for (i = 0; i<cols; i++) {
+		for ($i = 0; $i<$cols; $i++) {
 			echo '<input type="text" name="fname'.$i.'" placeholder="Название поля"> ';
 			echo '<select name="ftype'.$i.'" placeholder="Tип поля">'.$types.'</select> ';
 			echo '<input type="text" name="fsize'.$i.'" placeholder="Длина поля"> ';
