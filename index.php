f5ff069eb228e622479abb400317e4ff326cfd72
Nix2k <mihail.bokov@gmail.com>
2018-03-21 17:12:23 +0300

    0

--- index.php
+++ index.php
@@ -25,14 +25,9 @@
 					<th>Дополнительно</th>
 				</tr>";
 		foreach ($columns as $col) {
-			/*echo "<tr>
-					<td>$col['Field']</td>
-					<td>$col['Type']</td>
-					<td>$col['Null']</td>
-					<td>$col['Key']</td>
-					<td>$col['Default']</td>
-					<td>$col['Extra']</td>
-				</tr>";	*/
+			echo "<tr>
+					<td>".$col['Field']."</td>
+				</tr>";
 		}
 		echo "</table>";
 	}
